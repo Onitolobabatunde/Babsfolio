93efc6518ebd8f61b559fb24a8ea91e7d765f15b
Babatunde Onitolo <onitololaurence@gmail.com>
2024-04-28 09:13:25 +0100

    first commit

---
+++ constants/data.js
@@ -0,0 +1,109 @@
+const userData = {
+  githubUsername: "onitolobabatunde",
+  name: "Babatunde Onitolo",
+  designation: "Front-End Developer",
+  avatarUrl: "/Sam.jpg",
+  email: "babatunde.onitolo@hotmail.com",
+  phone: "+234 817 105 4092",
+  address: "Lagos, Nigeria.",
+  projects: [
+    {
+      title: "SkillHat",
+      link: "https://skillhat.ca",
+      imgUrl: "/Skillhat.jpg",
+    },
+    {
+      title: "Eugee Music",
+      link: "https://www.eugeemusic.com/",
+      imgUrl: "/eugee.jpg",
+    },
+    {
+      title: "Ez Dashboard",
+      link: "https://dashboard-31393.bubbleapps.io/version-test?debug_mode=true",
+      imgUrl: "/Dashboard.png",
+    },
+    {
+      title: "My Blog",
+      link: "https://my-blog-wheat-alpha.vercel.app",
+      imgUrl: "/Blog.png",
+    },
+    {
+      title: "Insight-Ways",
+      link: "https://insight-ways.com",
+      imgUrl: "/Financial.jpg",
+    },
+
+    {
+      title: "Baruch",
+      link: "http://baruch-nazarenebooks.com.ng",
+      imgUrl: "/Baruch.png",
+    },
+    {
+      title: "Job-Mobile App ",
+      link: "https://github.com/Onitolobabatunde/React_Native_Jobs",
+      imgUrl: "/jobs_app.jpg",
+    },
+    {
+      title: "Brass",
+      link: "https://bo-brass.bubbleapps.io/version-test",
+      imgUrl: "/brass.jpg",
+    },
+    {
+      title: "US. Food",
+      link: "https://deliverymarketplatform.bubbleapps.io/version-test",
+      imgUrl: "/USfood.png",
+    },
+  ],
+  about: {
+    title:
+      "I'm a software developer that loves building products and web applications that impact millions of lives",
+    description: [
+      `I've been developing full-stack application since I was 10 years old. I didn't know what full-stack meant at that time because the term was not coined back then. I'm so out of content right now that I'm literally crying writing this text since lorem text is so 2012 now.`,
+      `After learning HTML and struggling with CSS, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself and - for obvious reasons - if you knew bootstrap, you were cool.`,
+      `But now, I'm a legend. I've been coding in React, React Native, Bootstrap, Tailwind, AWS Cloud computing (In View) etc to name a few. I consider myself a Ninja developer I'm laughing right now.`,
+    ],
+    currentProject: "Babs Technologies",
+    currentProjectUrl: "#",
+  },
+  experience: [
+    {
+      title: "Founder and Developer",
+      company: "Babs Technologies",
+      year: "2023",
+      companyLink: "#",
+      desc: "Your last moment revision friend right before a Tech coding interview. Practice problems and get hired at top Product based companies.",
+    },
+    {
+      title: "Internship",
+      company: "Intimate Inc.",
+      year: "2021",
+      companyLink: "#",
+      desc: "Developed a dating app for meeting a new friends to chat with both local and international.",
+    },
+    {
+      title: "Graduation",
+      company: "Univesity of Lagos",
+      year: "2022",
+      companyLink: "https://upes.co.in",
+      desc: "Major in Computer Science and Engineering.",
+    },
+    {
+      title: "High School",
+      company: "Evaton Comprehensive College.",
+      year: "2015",
+      companyLink: "#",
+      desc: "Subjects being PCM - I barely survived with 93.6% marks aggregate. Flex fridays, fellas.",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1hbQ2peoVXAE8DBeeG4RDyTAm5FeuUIiN/view?usp=sharing",
+  socialLinks: {
+    instagram: "https://www.instagram.com/babs_onitolo",
+    twitter: "https://twitter.com/onitolo_babs",
+    linkedin: "https://linkedin.com/in/onitolobabatunde",
+    github: "https://github.com/Onitolobabatunde",
+    facebook: "https://web.facebook.com/onitololaurence",
+  },
+};
+
+export default userData;
